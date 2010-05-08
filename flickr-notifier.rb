48a815279b2ebb59f30cc3e697e766a82f2d0618
novemberkilo <navin@novemberkilo.com>
2010-05-08 22:59:36 +1000

    Now using FlickrawOptions for api_key and secret

--- flickr-notifier.rb
+++ flickr-notifier.rb
@@ -1,35 +1,58 @@
 =begin
 
-flickr-notifier(beta)
+flickr-notifier
 
-Gets recent activity against a user's flickr account and notifies the user via Growl.
+Gets recent activity against a user's flickr account and notifies the user via Growl
 
 Dependencies: The awesome flickraw gem and growlnotify
+You will need your own API key - see the README at 
+    http://github.com/novemberkilo/flickr-notifier/
 
 Author: Navin Keswani
 Date: April 2010
-License: [TBC] Per flickraw, growlnotify and the MIT license
+License: Per flickraw, growlnotify and the MIT license. See License section of the README at
+    http://github.com/novemberkilo/flickr-notifier/
 
 =end
+
+FlickRawOptions = {
+    "api_key" => "YOUR FLICKR API KEY HERE",
+    "shared_secret" => "YOUR FLICKR SHARED SECRET HERE",
+#    "auth_token" => "... Your saved token..."  # if you set this you will be automatically loggued
+    "lazyload" => true,     # This delay the loading of the library until the first call
+
+    # Proxy support. alternatively, you can use the http_proxy environment variable
+#    "proxy_host" => "proxy_host",
+#   "proxy_port" => "proxy_port",
+#    "proxy_user" => "proxy_user",
+#    "proxy_password" => "proxy_password",
+
+    "timeout" => 5,                # Set the request timeout
+#    "auth_token" => "SAVED_TOKEN"  # Set the initial token
+  }
 
 require 'rubygems'
 require 'flickraw'
 require 'logger'
 
-# Set up log files
-file = File.open('flickr-notifier_log.log', File::WRONLY | File::APPEND | File::CREAT)
+# Set up log file
+file = File.open('flickr-notifier-log.log', File::WRONLY | File::APPEND | File::CREAT)
 
 # Start the log over whenever the log exceeds 1 megabyte in size
 $LOG = Logger.new(file, 0, 1024 * 1024)
 
 $LOG.level = Logger::DEBUG
 
-FlickRaw.api_key = "38d132c8b4966e86987654dad17b656e"
-FlickRaw.shared_secret = "627dd83a8161fd83"
-
 AUTH_TOKEN_FILE = ".flickr_auth_token"
 
 def authenticate
+
+  # Follows flickraw's recommended authentication flow.
+  # Checks if an AUTH_TOKEN_FILE exists and extracts the 
+  # authentication token from it if it does. Else, follows
+  # flickraw's recipe for obtaining a frob and then a token 
+  # and writes the token to AUTH_TOKEN_FILE 
+
   if File.exist? AUTH_TOKEN_FILE
     auth_token = File.open(AUTH_TOKEN_FILE).gets
     auth = flickr.auth.checkToken :auth_token => auth_token
@@ -55,11 +78,14 @@ end
 
 def download_photo (photo_filename,photo_specs)
 
-  # Returns true if new file created else false
+  # Returns true if photo downloaded (i.e. a new file created in
+  # the ./.images directory, else false.
+  # Takes two arguments - photo_filename and photo_specs
   # Expects photo_filename to be a string representation
-  # of the path of the file.
-  # Expects json specification of the photo in photo_specs.
-  # Reuses a code snippet from DZone Snippets
+  # of the path of the file
+  # Expects json specification of the photo in photo_specs 
+  # URL parsing and file writing per a code snippet from DZone Snippets 
+
   unless File.exists?(photo_filename)
     begin
       url = URI.parse(FlickRaw.url_s(photo_specs))
@@ -80,18 +106,27 @@ def download_photo (photo_filename,photo_specs)
 end
 
 def notify (title='', message='', icon_filename =".images/Flickr-logo.jpg", sticky = false)
+
   # Tuck the growl notifications here - if we need to change the notification
   # mechanism then this method can be overloaded or modified to suit
   # puts "In notify with icon_filename #{icon_filename}"
+
    $LOG.add(Logger::DEBUG){"Calling growlnotify with message #{message}"}
   `growlnotify #{(sticky)? '-s':''} --image #{icon_filename} -m "#{message}" "#{title}"`
 end
 
 def get_recent_activity (timeframe)
-  # Expects timeframe - a string specifying the period over which to retrieve activity
+
+  # Expects timeframe - a string specifying the period 
+  # over which to retrieve activity.
+  # Compiles a notification of new comments made on the 
+  # user's photostream. 
+  # Only reports activity once - i.e. a recent comment will not be 
+  # replayed every time this method is called. 
+
   $LOG.add(Logger::DEBUG){"Checking recent activity"}
   unless defined? @displayed_comments
-    @displayed_comments=[]  # ids for comments that have been displayed
+    @displayed_comments=[]  # array to hold ids for comments that have been displayed
   end
 
   recent_activity = flickr.activity.userPhotos(:timeframe => timeframe)
@@ -104,29 +139,29 @@ def get_recent_activity (timeframe)
     $LOG.add(Logger::DEBUG){"Called flickr.activity - recent activity detected"}
     recent_activity.each do |x|
       message = ''
-      title = x.title
-      photo_id = x.id
-      photo_filename = ".images/#{photo_id}.jpg"
-      download_photo(photo_filename,x)
       x.activity.event.each do |z|
         case
         when z.type == "comment" then
         begin
             unless @displayed_comments.include? z.commentid
-              @displayed_comments.push z.commentid
+              @displayed_comments.push z.commentid 
               message << "Comment: " << z._content << "\n"
             end
         end
         when z.type == "fave" then
           message << "Added as a favorite\n"
         end
-        unless (message == '')
+        unless (message == '')   # Which can happen if recent_activity is neither a comment nor a fave
           message << "User: " << z.username << "\n"
           time_added = Time.at(z.dateadded.to_i)
           message << time_added.to_s << "\n"
         end
       end
       unless (message == '')
+        title = x.title                              # Get the photo's title
+        photo_id = x.id                              
+        photo_filename = ".images/#{photo_id}.jpg"   # Filename of the thumbnail
+        download_photo(photo_filename,x)
         notify(title, message, photo_filename,true)
       end
     end
@@ -134,6 +169,12 @@ def get_recent_activity (timeframe)
 end
 
 def get_stats(date)
+
+  # Expects date - a string of format "YYYY-MM-DD"
+  # Calls flickr.stats.getTotalViews(date) to pick up 
+  # summary of activity and flickr.stats.getPopularPhotos(date)
+  # to get 5 most popular photos
+
   $LOG.add(Logger::DEBUG){"Checking stats for date #{date}"}
   title = "Flickr stream views for today"
   all_quiet_message = "All quiet at your photostream"
@@ -177,7 +218,7 @@ begin
   sleep_period = 15 # Recommend not making this smaller than 15
   # Flickr requests that the recent_activity method not be called more
   # than once an hour so any more than 4 times in an hour will be very cheeky!
-  # Report on view counts once every hour (within the first quarter)
+  # Report on view counts once every hour (within the first quarter of the hour)
   
   # Set up the .image directory unless it already exists
   Dir.mkdir(".images") if Dir.glob(".images")==[]
@@ -202,6 +243,7 @@ begin
     $LOG.add(Logger::ERROR){"Fatal error - in rescue block"}
     puts "Oops, something went wrong with flickr-notifier."
     puts "Is Growl running? (Check system preferences)"
+    puts "Did you put your flickr API key and secret into flickr-notifier.rb?"
     puts "Did your internet connection drop out?"
 end
 
